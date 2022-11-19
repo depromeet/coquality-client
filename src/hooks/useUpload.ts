bf0666dff372c4dedd61aa50a4882e7c8e339b37
kimhn0605 <kimhn200605@gmail.com>
2022-11-19 16:21:59 +0900

    feat: 마이페이지 퍼블리싱 (#3)
    * feat(mypage): 마이페이지 퍼블리싱
    
    * feat(mypage): 마이페이지 수정화면 퍼블리싱
    
    * chore: package-lock.json 추가
    
    * feat(mypage): 수정 버튼 클릭 시 페이지 이동
    
    * feat(hooks): 이미지 업로드 관련 hook 추가
    
    * style(mypage): 이미지 업로드 버튼 커스텀

---
+++ src/hooks/useUpload.ts
@@ -0,0 +1,26 @@
+import { useState, useCallback } from 'react'
+
+const useUpload = () => {
+  const [file, setFile] = useState<File | null>(null)
+  const upload = useCallback(() => {
+    const promise = new Promise<File | null>((resolve, reject) => {
+      const input = document.createElement('input')
+      input.accept = 'image/*'
+
+      const timeout = setTimeout(reject, 1000 * 60 * 3)
+      input.type = 'file'
+      input.onchange = () => {
+        clearTimeout(timeout)
+        if (!input.files) return reject()
+        const file = input.files[0]
+        setFile(file)
+        resolve(file)
+      }
+      input.click()
+    })
+    return promise
+  }, [])
+  return [upload, file] as [typeof upload, typeof file]
+}
+
+  export default useUpload
