67f0148b756702b3c50d016542632fc4ab3d03dc
정시원 <sebastianrcnt@gmail.com>
2022-11-19 14:52:11 +0900

    feat: 작가 섹션 추가

--- src/components/[username]/[slug]/index.tsx
+++ src/components/[username]/[slug]/index.tsx
@@ -85,37 +85,44 @@ const Slug: React.FC<Props> = ({}) => {
   }, []);
 
   return (
-    <Container>
-      <Heading fontSize="xl" mb="2">
-        {post?.title}
-      </Heading>
-      <Heading fontSize="md" mb="2">
-        {post?.summary}
-      </Heading>
-      <Flex display="flex" flexDirection="row">
-        <Box mr="2">view 356</Box>
-        <Box>2022.10.25</Box>
-      </Flex>
-      <Divider></Divider>
-      <Text py="8">{post?.contents}</Text>
-      <Divider></Divider>
-      <Text>20개의 댓글</Text>
-      <Flex p="2" direction="column" gap="2">
-        <Flex direction="row">
-          <Textarea
-            placeholder="댓글 작성이 어렵다면 간단한 이모티콘으로 생각을 표현해보세요!"
-            border="none"
-          />
+    <Flex direction="row" p="10">
+      <Flex direction="column" flex="2">
+        <Heading fontSize="xl" mb="2">
+          {post?.title}
+        </Heading>
+        <Heading fontSize="md" mb="2">
+          {post?.summary}
+        </Heading>
+        <Flex display="flex" flexDirection="row">
+          <Box mr="2">view 356</Box>
+          <Box>2022.10.25</Box>
         </Flex>
-        <Divider />
-        <Flex direction="row" justifyContent="flex-end">
-          <Button>작성 완료</Button>
+        <Divider></Divider>
+        <Text py="8">{post?.contents}</Text>
+        <Divider></Divider>
+        <Text>20개의 댓글</Text>
+        <Flex py="4" direction="column" gap="2">
+          <Flex direction="row">
+            <Textarea
+              placeholder="댓글 작성이 어렵다면 간단한 이모티콘으로 생각을 표현해보세요!"
+              border="none"
+            />
+          </Flex>
+          <Divider />
+          <Flex direction="row" justifyContent="flex-end">
+            <Button>작성 완료</Button>
+          </Flex>
         </Flex>
+        {comments.map((comment, index) => (
+          <Comment key={index} {...comment} />
+        ))}
       </Flex>
-      {comments.map((comment, index) => (
-        <Comment key={index} {...comment} />
-      ))}
-    </Container>
+      <Flex direction="column" flex="1" ml="2">
+        <Avatar src="https://picsum.photos/200/200" size="xl"></Avatar>
+        <Heading size="md">미진</Heading>
+        <Text>3년차 공유 모빌리티 서비스 디자이너 ***입니다.</Text>
+      </Flex>
+    </Flex>
   );
 };
 
