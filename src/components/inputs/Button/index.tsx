c4012cbc06538b3c7c4dfb0c14968e47f58d8bfc
front_이상민 <lee.sangmin@cashwalk.io>
2022-12-10 17:26:22 +0900

    feat: css props가 동작하도록 수정합니다.

--- src/components/inputs/Button/index.tsx
+++ src/components/inputs/Button/index.tsx
@@ -1,9 +1,54 @@
-import React from "react"
+import React, { ButtonHTMLAttributes, ReactNode } from "react"
+import styled from "@emotion/styled"
+import { colors } from "@constants/colors"
+import { css } from "@emotion/react"
 
-type Props = {}
+const colorSet = {
+  primary: {
+    default: colors.primary600,
+    hover: colors.primary600,
+    disabled: colors.grey300,
+  },
+}
 
-const Button: React.FC<Props> = () => {
-  return <div>Button</div>
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string
+  color?: "primary"
+  variant?: "contained" | "outline"
+}
+
+const Button: React.FC<Props> = ({
+  className,
+  color = "primary",
+  variant = "contained",
+  children,
+  ...props
+}) => {
+  return (
+    <StyledWrapper
+      className={["common-h5-sb", className].join(" ")}
+      css={css`
+        background-color: ${colors.primary500};
+      `}
+      {...props}
+    >
+      {children}
+    </StyledWrapper>
+  )
 }
 
 export default Button
+
+const StyledWrapper = styled.button`
+  background-color: ${colors.primary500};
+  padding: 6px 30px;
+  border-radius: 30px;
+  gap: 5px;
+  &:hover {
+    background-color: ${colors.primary600};
+  }
+  &:disabled {
+    color: ${colors.grey500};
+    background-color: ${colors.grey300};
+  }
+`
