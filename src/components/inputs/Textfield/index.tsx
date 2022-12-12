0a000a8a571535d0bd083d2a3c8b14df29e6c82e
Lee Sang Min <morethanmin.dev@gmail.com>
2022-12-12 20:51:41 +0900

    feat: 내 정보 수정 페이지를 퍼블리싱합니다. (#34)

--- src/components/inputs/Textfield/index.tsx
+++ src/components/inputs/Textfield/index.tsx
@@ -1,9 +1,67 @@
-import React from "react"
+import { colors } from "@constants/colors"
+import styled from "@emotion/styled"
 
-type Props = {}
+import React, { InputHTMLAttributes, ReactNode } from "react"
 
-const Textfield: React.FC<Props> = () => {
-  return <div>Textfield</div>
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label?: Exclude<ReactNode, boolean | null | undefined>
+  error?: string
+  hint?: string
+  focus?: boolean
+}
+
+const Textfield: React.FC<Props> = ({
+  className,
+  label,
+  hint,
+  error,
+  focus = true,
+  ...props
+}) => {
+  return (
+    <StyledWrapper className={className}>
+      {label && <div className="label common-h6-sb">{label}</div>}
+      <input
+        type="text"
+        spellCheck="false"
+        autoComplete="off"
+        data-focus={focus}
+        data-error={!!error}
+        {...props}
+      />
+      {hint && <div className="hint">{hint}</div>}
+      {error && <div className="error">{error}</div>}
+    </StyledWrapper>
+  )
 }
 
 export default Textfield
+
+const StyledWrapper = styled.div`
+  .label {
+  }
+  .hint,
+  .error {
+    font-weight: 400;
+    margin-top: 5px;
+    font-size: 12px;
+    &.hint {
+    }
+    &.error {
+    }
+  }
+  input {
+    width: 100%;
+    border: none;
+    outline: none;
+    border-bottom: 1px solid ${colors.grey400};
+    padding: 12px 0px;
+    display: flex;
+    align-items: center;
+    background-color: transparent;
+    &[data-focus="true"] {
+    }
+    &[data-error="true"] {
+    }
+  }
+`
