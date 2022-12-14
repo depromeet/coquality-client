6e6c85aff724025989cadcb599477573747a888c
정시원 <sebastianrcnt@gmail.com>
2022-12-14 21:20:42 +0900

    test(feat/21): add tests for getCommentsOfPost

--- src/libs/api/comments.ts
+++ src/libs/api/comments.ts
@@ -1,12 +1,20 @@
 import Repository from "./repository"
 
-interface IComment {}
+interface IComment {
+  id: number
+  contents: string
+  userId: number
+  postId: number
+  createdAt: string
+}
 
 export class CommentsRepository extends Repository {
   public async getCommentsOfPost(postId: number): Promise<IComment[]> {
     const response = await this.client.get(`/comments/${postId}`, {
       headers: { AUTH: this.authToken },
     })
+
+    return response.data.data
 
     // TODO: 백엔드 문의(500)
     // TODO: add response types
