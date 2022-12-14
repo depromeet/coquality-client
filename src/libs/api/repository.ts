9dda1a74568b4b2bf7abade4079cc22dcb0b6eac
Lee Sang Min <morethanmin.dev@gmail.com>
2022-12-14 23:25:58 +0900

    fix: 빌드 오류를 해결하고 yarn.lock을 제거합니다. (#40)

--- src/libs/api/repository.ts
+++ src/libs/api/repository.ts
@@ -3,6 +3,6 @@ import { AxiosInstance } from "axios"
 export default abstract class Repository {
   constructor(
     protected readonly client: AxiosInstance,
-    protected authToken: string
+    protected authToken?: string
   ) {}
 }
