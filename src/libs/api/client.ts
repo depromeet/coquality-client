15265d1676cb549bb910e836beb2b69746c1d350
morethanmin <mini4614@gmail.com>
2022-12-17 15:12:44 +0900

    feat: API_URL env를 추가합니다.

--- src/libs/api/client.ts
+++ src/libs/api/client.ts
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const coqualityAxiosClient = axios.create({
   baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
     "http://ec2-15-165-142-212.ap-northeast-2.compute.amazonaws.com:8080/api/v1",
 })
 
